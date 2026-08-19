effcb147cd2e072e72ae22904516c7df1a3b7694
Cornelius <8294697+CorneliusCornbread@users.noreply.github.com>
2026-08-18 20:01:37 -0500

    Restore moving leaf colliders

--- common/src/main/java/com/farcr/treephysics/mixin/leaf_collision/VoxelNeighborhoodStateMixin.java
+++ common/src/main/java/com/farcr/treephysics/mixin/leaf_collision/VoxelNeighborhoodStateMixin.java
@@ -1,5 +1,6 @@
 package com.farcr.treephysics.mixin.leaf_collision;
 
+import com.farcr.treephysics.api.util.TreeUtil;
 import com.farcr.treephysics.index.TreePhysicsConfig;
 import com.farcr.treephysics.mixin.accessors.LevelAcceleratorAccessor;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
@@ -8,7 +9,6 @@ import dev.ryanhcode.sable.Sable;
 import dev.ryanhcode.sable.physics.chunk.VoxelNeighborhoodState;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.block.LeavesBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -18,7 +18,7 @@ public class VoxelNeighborhoodStateMixin {
 
     @WrapOperation(method = "getState", at = @At(value = "INVOKE", target = "Ldev/ryanhcode/sable/physics/chunk/VoxelNeighborhoodState;isSolid(Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;)Z"))
     private static boolean treephysics$isSolid(BlockGetter blockGetter, BlockPos pos, BlockState state, Operation<Boolean> original) {
-        if(state.getBlock() instanceof LeavesBlock && !TreePhysicsConfig.STATIC_LEAF_COLLISION.get() && blockGetter instanceof LevelAcceleratorAccessor accessor) {
+        if(TreeUtil.isLeaf(state) && !TreePhysicsConfig.STATIC_LEAF_COLLISION.get() && blockGetter instanceof LevelAcceleratorAccessor accessor) {
             return Sable.HELPER.getContaining(accessor.getLevel(), pos) != null;
         }
         return original.call(blockGetter, pos, state);
